dd3d8867fc489249147454e9efd8f1eb789f1764
Kevin Hoang <khoang1@uncc.edu>
2017-02-24 16:54:23 +0000

    Done with section 4.3

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,13 @@
 # Seed the ROttenPotatoes B with some movies.
 more_movies = [
-  {:title => 'Aladdin', :rating => 'G', release_date => '25-Nov-1992'}
-  {:title => 'When Harry Met Sally', :rating => 'R', release_date => '21-Jul-1982'}
-  {:title => 'The Help', :rating => 'PG-13', release_date => '10-Aug-2011'}
-  {:title => 'Rauders if the Lost Ark', :rating => 'PG', release_date => '12-Jun-1981'}
+  {:title => 'Aladdin', :rating => 'G', 
+    :release_date => '25-Nov-1992'},
+  {:title => 'When Harry Met Sally', :rating => 'R', 
+    :release_date => '21-Jul-1982'},
+  {:title => 'The Help', :rating => 'PG-13', 
+    :release_date => '10-Aug-2011'},
+  {:title => 'Rauders if the Lost Ark', :rating => 'PG', 
+    :release_date => '12-Jun-1981'}
   
   
   ]
